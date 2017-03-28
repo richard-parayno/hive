22720d6fe115b11f7ed080c7d47b290c907903ff
Richard Parayno <broodheart@gmail.com>
2017-03-28 22:07:59 +0800

    updated schedule-termite

--- schedule-treatment/schedule-termite.php
+++ schedule-treatment/schedule-termite.php
@@ -18,6 +18,7 @@
   <script src="../bower_components/semantic-ui-calendar/dist/calendar.min.js"></script>
   <?php 
     ob_start();
+    session_start();
     ?>
 </head>
 
@@ -26,9 +27,6 @@
   <!-- SIDEBAR START -->
   <div class="ui inverted left vertical sidebar menu">
     <div class="item">
-      <a class="ui logo icon image" href="#">
-        <img src="assets/logo.png">
-      </a>
       <a href="#">
         <b class="centered">Hive Resource Management System</b>
       </a>
@@ -36,16 +34,16 @@
     <a class="item" href="../sales-index.php">
       <i class="home icon"></i> Sales Dashboard
     </a>
-    <a class="item" href="sales-orders/create-service-requests.php">
+    <a class="item" href="../sales-orders/create-service-requests.php">
       Create Service Requests
     </a>
-    <a class="item" href="sales-orders/reschedule-service-requests.php">
+    <a class="item" href="../sales-orders/reschedule-service-requests.php">
       Reschedule Service Requests
     </a>
-    <a class="item" href="sales-chemicals/chemical.php">
+    <a class="item" href="../sales-chemicals/chemical.php">
       Chemical Inventory
     </a>
-    <a class="item" href="sales-clients/clients-list.php">
+    <a class="item" href="../sales-clients/clients-list.php">
       View Clients
     </a>
     <div class="item">
@@ -53,27 +51,27 @@
         Reports
       </div>
       <div class="menu">
-        <a class="item" href="sales-reports/termite-treatment-report.php">
+        <a class="item" href="../sales-reports/termite-treatment-report.php">
           Termite Treatment Report
         </a>
-        <a class="item" href="sales-reports/household-treatment-report.php">
+        <a class="item" href="../sales-reports/household-treatment-report.php">
           Household Treatment Report
         </a>
-        <a class="item" href="sales-reports/general-services-report.php">
+        <a class="item" href="../sales-reports/general-services-report.php">
           General Services Report
         </a>
-        <a class="item" href="sales-reports/list-of-oculars-report.php">
+        <a class="item" href="../sales-reports/list-of-oculars-report.php">
           List of Oculars Report
         </a>
-        <a class="item" href="sales-reports/accomplished-oculars-report.php">
+        <a class="item" href="../sales-reports/accomplished-oculars-report.php">
           Accomplished Oculars Report
         </a>
-        <a class="item" href="sales-resports/unaccomplished-oculars-report.php">
+        <a class="item" href="../sales-resports/unaccomplished-oculars-report.php">
           Unaccomplished Oculars Report
         </a>
       </div>
     </div>
-    <a class="item" href="login.php">
+    <a class="item" href="../login.php">
       Log Out
     </a>
   </div>
@@ -82,28 +80,28 @@
 
   <!-- MAIN CONTENT START -->
   <div class="pusher">
-    <!-- TOP BAR START-->
-    <div class="sixteen wide column">
-      <div class="ui top menu">
-        <a class="ui item launch">
-          <i class="sidebar icon"></i> Menu
-        </a>
-        <div class="item">
-          Sales and Marketing Department
-        </div>
-        <div class="item">
-          <div class="ui breadcrumb">
-            <a class="section" href="../sales-index.php">Sales Dashboard</a>
-            <i class="right angle icon divider"></i>
-            <a class="section" href="../sales-index.php">Schedule Termite Treatment</div>
-            <i class="right angle icon divider"></i>
-            <div class="active section">Confirm Termite Treatment</div>
-					</div>
-        </div>
-        <div class="right menu ">
-          <a class="ui labeled item notifications">
+    <div class="pusher">
+      <!-- TOP BAR START-->
+      <div class="sixteen wide column">
+        <div class="ui top menu" id="topbar">
+          <a class="ui item launch">
+            <i class="sidebar icon"></i> Menu
+          </a>
+          <div class="item">
+            Sales and Marketing Department
+          </div>
+          <div class="item">
+            <div class="ui breadcrumb">
+              <a class="section" href="../sales-index.php">Sales Dashboard</a>
+              <i class="right angle icon divider"></i>
+              <a class="section" href="../sales-index.php">Schedule Termite Treatment</a>
+              <i class="right angle icon divider"></i>
+              <div class="active section">Schedule Termite Treatment</div>
+            </div>
+          </div>
+          <div class="right menu ">
+            <a class="ui labeled item notifications">
             Notifications
-            <div class="ui basic red circular label">10</div>
           </a>
           </div>
         </div>
@@ -139,9 +137,9 @@
         		$flag=0;
 			if (isset($_POST['accept'])){
        require_once('../mysql_connect.php');
-				 $toSchedule = $_POST['termite-oculars'] ;
+				 $toSchedule = $_POST['termiteoculars'] ;
 			//	$toSchedule =4;
-       $query="Select * from occular_visits where occular_id = '{$toSchedule}' ";
+        $query="Select * from occular_visits where occular_id = '{$toSchedule}' ";
 				$wer =  mysqli_query($dbc,$query); 
 				$roww= mysqli_fetch_array($wer, MYSQLI_ASSOC);  
 				$callername= $roww['CustomerID']; 
@@ -151,15 +149,12 @@
         $run= mysqli_query($dbc,$getAreaType);
         $getIt=mysqli_fetch_array($run,MYSQLI_ASSOC);
         $stype= $getIt['Area_type'];
-				// $dateTimeclass = new DateTime ($_SESSION['date']);
-              	// $endDate= new DateTime($_SESSION['date']);
-           //$dt = new DateTime("2015-11-01 00:00:00"
-				$dateTimeclass =  new DateTime ($_POST['initial-treat-termite']);
-          $endDate= new DateTime ($_POST['initial-treat-termite']);
-      //  echo "WTF IS RON" .$stype;
-        	 $dateTimeclass->format('Y-m-d');
-        	$endDate->format('Y-m-d');
-          $datetrial = new DateInterval('P1Y');  	
+			
+				$dateTimeclass =  new DateTime ($_POST['initialtreattermite']);
+        $endDate= new DateTime ($_POST['initialtreattermite']);
+        $dateTimeclass->format('Y-m-d');
+        $endDate->format('Y-m-d');
+        $datetrial = new DateInterval('P1Y');  	
 				$endDate->add($datetrial); 
 				$counter=0;
 				$datetrial = new DateInterval('P1Y');  	
@@ -189,7 +184,7 @@
             <!-- Cancel Button -->
             <?php
         		if (isset($_POST['cancel'])){
-        			header("Location:  http://".$_SERVER['HTTP_HOST']. dirname($_SERVER['PHP_SELF']). "../../sales-index.php");
+        			header("Location:  http://".$_SERVER['HTTP_HOST']. dirname($_SERVER['PHP_SELF']). "/../sales-index.php");
 
         		}
 
@@ -197,56 +192,56 @@
               <!-- Cancel Button end -->
               <!-- CONFIRM SCHEDULE START -->
               <div class="sixteen wide centered column">
-                <div class="ui center aligned basic segment">
+                <div class="ui center aligned segment">
                   <div class="ui horizontal divider">
-                    Occular Report Details
+                    Ocular Report Details
                   </div>
                   <?php 
-  			require_once('../mysql_connect.php');
-  			//('{$aetype}'
-  			// $toSchedule = $_SESSION['termiteocular'] ;
-  			// remove $toshed=4 once session from sales-index works :) 
-  			$toSchedule = $_POST['termite-oculars']; 
-  			$query="Select * from occular_visits where occular_id = '{$toSchedule}' "; 
-  			$run =  mysqli_query($dbc,$query);	
-			$getData=mysqli_fetch_array($run,MYSQLI_ASSOC);
-			//'{$getData['CustomerID']}'
-			$getCustomerNameQuery= "Select Name from customer where customerID = '{$getData['CustomerID']}'";
-			$run2=mysqli_query($dbc,$getCustomerNameQuery);
-			$getdata2=mysqli_fetch_array($run2,MYSQLI_ASSOC);
-			//echo "Peter is " . $age['Peter'] . " years old.";
-			echo "<b>Customer Name:</b> " . $getdata2['Name']; 
-			echo "<br>";
-			echo "<b>Degree of Infestation:</b> " .$getData['Area_Infection'];
-			echo "<br>";
-			$date = new DateTime ($getData['Date']);
-			// $dateTimeclass = new DateTime ($_POST['Date']); 
-			//$dateTimeclass->format('Y-m-d')
-			echo "<b>Date of Occular: </b>" .$date->format('m-d-Y');
-			echo "<br>";
-			echo "<b>Area Size:     </b>" .$getData['Area_Size'];
-			echo "<br>";	
-			echo "<b>Looking For at Site: </b>" .$getData['LF_At_Site'];
-			echo "<br>";	
-			echo "<b> Remarks: </b>" .$getData['Remarks'];
-			echo "<br>";	
-			echo "<b>Findings: </b>" .$getData['Findings'];
-			echo "<br>";		
-			$getEmployee= "Select Name from employee where EmployeeNo = '{$getData['SupervisedBy']}'";
-			$run3=mysqli_query($dbc,$getEmployee);
-			$getdata3=mysqli_fetch_array($run3,MYSQLI_ASSOC);
-			echo "<b>Occular Representative: </b>" .$getdata3['Name'];
-			echo "<br>";
-  			?>
+                    require_once('../mysql_connect.php');
+                    //('{$aetype}'
+                    // $toSchedule = $_SESSION['termiteocular'] ;
+                    // remove $toshed=4 once session from sales-index works :) 
+                    $toSchedule = $_POST['termiteoculars']; 
+                    $query="Select * from occular_visits where occular_id = '{$toSchedule}' "; 
+                    $run =  mysqli_query($dbc,$query);	
+                    $getData=mysqli_fetch_array($run,MYSQLI_ASSOC);
+                    //'{$getData['CustomerID']}'
+                    $getCustomerNameQuery= "Select Name from customer where customerID = '{$getData['CustomerID']}'";
+                    $run2=mysqli_query($dbc,$getCustomerNameQuery);
+                    $getdata2=mysqli_fetch_array($run2,MYSQLI_ASSOC);
+                    //echo "Peter is " . $age['Peter'] . " years old.";
+                    echo "<b>Customer Name:</b> " . $getdata2['Name']; 
+                    echo "<br>";
+                    echo "<b>Degree of Infestation:</b> " .$getData['Area_Infection'];
+                    echo "<br>";
+                    $date = new DateTime ($getData['Date']);
+                    // $dateTimeclass = new DateTime ($_POST['Date']); 
+                    //$dateTimeclass->format('Y-m-d')
+                    echo "<b>Date of Occular: </b>" .$date->format('m-d-Y');
+                    echo "<br>";
+                    echo "<b>Area Size:     </b>" .$getData['Area_Size'];
+                    echo "<br>";	
+                    echo "<b>Looking For at Site: </b>" .$getData['LF_At_Site'];
+                    echo "<br>";	
+                    echo "<b> Remarks: </b>" .$getData['Remarks'];
+                    echo "<br>";	
+                    echo "<b>Findings: </b>" .$getData['Findings'];
+                    echo "<br>";		
+                    $getEmployee= "Select Name from employee where EmployeeNo = '{$getData['SupervisedBy']}'";
+                    $run3=mysqli_query($dbc,$getEmployee);
+                    $getdata3=mysqli_fetch_array($run3,MYSQLI_ASSOC);
+                    echo "<b>Occular Representative: </b>" .$getdata3['Name'];
+                    echo "<br>";
+                  ?>
                   <br> </br>
 
                   <div class="ui horizontal divider">
                     <h3 class="ui header">Termite Treatment Schedule Details</h3>
                   </div>
-                  <form class="ui form" method="post" action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF "]);?>">
+                  <form class="ui form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
                     <?php 
-              		 $toSchedule = new DateTime ($_POST['initial-treat-termite']);
-              		  $endDate= new DateTime($_POST['initial-treat-termite']);
+              		 $toSchedule = new DateTime ($_POST['initialtreattermite']);
+              		  $endDate= new DateTime($_POST['initialtreattermite']);
               		 // $toSchedule  = new DateTime("2018-12-01 00:00:00");
                   // $endDate= new DateTime("2019-12-01 00:00:00");
               		  //$dt = new DateTime("2015-11-01 00:00:00"
@@ -291,32 +286,6 @@
               </div>
         </div>
         <!-- CONFIRM SCHEDULE END -->
-
-        <!-- HOUSEHOLD MODAL START -->
-        <div class="ui modal household">
-          <i class="close icon"></i>
-          <div class="header">
-            Schedule Household Treatment
-          </div>
-          <div class="content">
-            <div class="description">
-              <p>Supervisor: </p>
-              <p>Date of Initial Treatment: </p>
-              <p>Account Executive: </p>
-              <p>Monthly Date of Service: </p>
-            </div>
-          </div>
-          <div class="actions">
-            <div class="ui black deny button">
-              Close
-            </div>
-            <div class="ui positive right labeled icon button">
-              Accept
-              <i class="checkmark icon"></i>
-            </div>
-          </div>
-        </div>
-        <!-- HOUSEHOLD MODAL END -->
 
       </div>
     </div>
